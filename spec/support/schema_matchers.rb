e1a41a2290abc7ec988592e9ceab28ee7c980733
Arlston Fernandes <gpg@arlston.com>
2019-10-11 15:20:34 +0100

    Test ProsecutionCaseIdentifier against provided schema

--- spec/support/schema_matchers.rb
+++ spec/support/schema_matchers.rb
@@ -1,6 +1,6 @@
 # set up schema matchers
 RSpec.configure do |config|
   config.include JSON::SchemaMatchers
-  config.json_schemas[:prosecution_case_identifier] = 'spec/support/schemas/prosecutionCaseIdentifier.json'
   config.json_schemas[:address] = 'spec/support/schemas/address.json'
+  config.json_schemas[:prosecution_case_identifier] = 'spec/support/schemas/prosecutionCaseIdentifier.json'
 end

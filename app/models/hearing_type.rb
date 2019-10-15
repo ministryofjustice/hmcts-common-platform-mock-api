db64c844b984871c23e25c3c1e3ffbc9f43d38b7
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-15 11:00:18 +0100

    Add HearingType model and specs

---
+++ app/models/hearing_type.rb
@@ -0,0 +1,12 @@
+class HearingType < ApplicationRecord
+    validates :description, presence: true
+    validates :code, presence: true
+
+    def to_builder
+      Jbuilder.new do |hearing_type|
+        hearing_type.id id
+        hearing_type.description description
+        hearing_type.code code
+      end
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_151856) do
+ActiveRecord::Schema.define(version: 2019_10_15_094744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -83,6 +83,13 @@ ActiveRecord::Schema.define(version: 2019_10_14_151856) do
     t.uuid "selfDefinedEthnicityId"
     t.string "selfDefinedEthnicityCode"
     t.string "selfDefinedEthnicityDescription"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "hearing_types", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "description"
+    t.string "code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

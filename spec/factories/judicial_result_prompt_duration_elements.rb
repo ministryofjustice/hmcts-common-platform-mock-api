1e0f72412e4cb0aa0ff4b187ca529b798252dd0e
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-15 15:27:38 +0100

    Add JudicialResultPromptDurationElement model and specs

---
+++ spec/factories/judicial_result_prompt_duration_elements.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :judicial_result_prompt_duration_element do
+    primaryDurationUnit { "MyString" }
+    primaryDurationValue { 1 }
+    primaryLabel { "MyString" }
+    secondaryDurationUnit { "MyString" }
+    secondaryDurationValue { 1 }
+    tertiaryDurationUnit { "MyString" }
+    tertiaryDurationValue { "MyString" }
+    durationStartDate { "2019-10-15 13:15:32" }
+    durationEndDate { "2019-10-15 13:15:32" }
+  end
+end

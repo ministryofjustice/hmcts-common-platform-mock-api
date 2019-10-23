a8f8a8696d9d7573fb34500de26366baa357f192
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-23 15:17:44 +0100

    Add .is_greater_than check to numericality validation

--- spec/models/court_application_outcome_type_spec.rb
+++ spec/models/court_application_outcome_type_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe CourtApplicationOutcomeType, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:description) }
-    it { should validate_numericality_of(:sequence) }
+    it { should validate_numericality_of(:sequence).is_greater_than(0) }
   end
 
   context 'hmcts schema' do

66e6e9b4bfb27f523b4ab4f4de913bafd7b95bc9
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-25 16:21:12 +0100

    Add shared context schema spec for Verdict

--- spec/models/verdict_spec.rb
+++ spec/models/verdict_spec.rb
@@ -3,6 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Verdict, type: :model do
+  let(:verdict) { FactoryBot.create(:verdict) }
+  let(:json_schema) { :verdict }
+
+  subject { verdict }
+
   describe 'associations' do
     it { should belong_to(:verdict_type).class_name('VerdictType') }
     it { should belong_to(:jurors).class_name('Jurors').optional }
@@ -16,9 +21,6 @@ RSpec.describe Verdict, type: :model do
     it { should validate_presence_of(:verdict_type) }
   end
 
-  let(:verdict) { FactoryBot.create(:verdict) }
+  it_has_behaviour 'conforming to valid schema'
 
-  it 'matches the given schema' do
-    expect(verdict.to_builder.target!).to match_json_schema(:verdict)
-  end
 end

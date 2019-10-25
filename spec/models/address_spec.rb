2f83b3c760a50ea6dace6fb267988248c030c9bd
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-25 11:36:18 +0100

    Add shared context schema spec for Address

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -3,13 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
+
+  let(:address) { FactoryBot.create(:address) }
+
+  let(:json_schema) { :address }
+
+  subject { address }
+
   describe 'validations' do
     it { should validate_presence_of(:address1) }
   end
 
-  let(:address) { FactoryBot.create(:address) }
+  it_has_behaviour 'conforming to valid schema'
 
-  it 'matches the given schema' do
-    expect(address.to_builder.target!).to match_json_schema(:address)
-  end
 end

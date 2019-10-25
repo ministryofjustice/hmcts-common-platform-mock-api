cbd85f9f0d13e1f320c2345b231b9537d047d2a6
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-25 11:49:07 +0100

    Add shared context schema spec for ContactNumber

--- spec/models/contact_number_spec.rb
+++ spec/models/contact_number_spec.rb
@@ -5,7 +5,10 @@ require 'rails_helper'
 RSpec.describe ContactNumber, type: :model do
   let(:contact_number) { FactoryBot.create(:contact_number) }
 
-  it 'matches the given schema' do
-    expect(contact_number.to_builder.target!).to match_json_schema(:contact_number)
-  end
+  let(:json_schema) { :contact_number }
+
+  subject { contact_number }
+
+  it_has_behaviour 'conforming to valid schema'
+
 end

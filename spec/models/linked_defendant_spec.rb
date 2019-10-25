da9c2dd728a18535a35056b507148386a25fee0b
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-25 15:43:47 +0100

    Add shared context schema spec for LinkedDefendant

--- spec/models/linked_defendant_spec.rb
+++ spec/models/linked_defendant_spec.rb
@@ -3,14 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe LinkedDefendant, type: :model do
+  let(:linked_defendant) { FactoryBot.create(:linked_defendant) }
+
+  let(:json_schema) { :linked_defendant }
+
+  subject { linked_defendant }
+
   describe 'validations' do
     it { should validate_presence_of(:prosecutionCaseId) }
     it { should validate_presence_of(:defendantId) }
   end
 
-  let(:linked_defendant) { FactoryBot.create(:linked_defendant) }
-
-  it 'matches the given schema' do
-    expect(linked_defendant.to_builder.target!).to match_json_schema(:linked_defendant)
-  end
+  it_has_behaviour 'conforming to valid schema'
 end

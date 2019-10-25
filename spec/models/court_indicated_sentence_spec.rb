58db0ce0735ad7cf0f17dbabfb48aae81913a908
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-25 14:24:30 +0100

    Add shared context schema spec for CourtIndicatedSentence

--- spec/models/court_indicated_sentence_spec.rb
+++ spec/models/court_indicated_sentence_spec.rb
@@ -3,12 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe CourtIndicatedSentence, type: :model do
+  let(:court_indicated_sentence) { FactoryBot.create(:court_indicated_sentence) }
+
+  let(:json_schema) { :court_indicated_sentence }
+
+  subject { court_indicated_sentence }
+
   it { should validate_presence_of(:courtIndicatedSentenceTypeId) }
   it { should validate_presence_of(:courtIndicatedSentenceDescription) }
 
-  let(:court_indicated_sentence) { FactoryBot.create(:court_indicated_sentence) }
-
-  it 'matches the given schema' do
-    expect(court_indicated_sentence.to_builder.target!).to match_json_schema(:court_indicated_sentence)
-  end
+  it_has_behaviour 'conforming to valid schema'
 end

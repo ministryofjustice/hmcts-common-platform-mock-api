7e481e7b236291a96dc2f628da2ed420ad25d60b
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-10-25 14:31:55 +0100

    Add shared context schema spec for DelegatedPowers

--- spec/models/delegated_powers_spec.rb
+++ spec/models/delegated_powers_spec.rb
@@ -3,13 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe DelegatedPowers, type: :model do
+  let(:delegated_powers) { FactoryBot.create(:delegated_powers) }
+
+  let(:json_schema) { :delegated_powers }
+
+  subject { delegated_powers }
+
   it { should validate_presence_of(:userId) }
   it { should validate_presence_of(:firstName) }
   it { should validate_presence_of(:lastName) }
 
-  let(:delegated_powers) { FactoryBot.create(:delegated_powers) }
+  it_has_behaviour 'conforming to valid schema'
 
-  it 'matches the given schema' do
-    expect(delegated_powers.to_builder.target!).to match_json_schema(:delegated_powers)
-  end
 end

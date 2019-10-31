d02126040d16a6d39f764077426ac483f423afea
Arlston Fernandes <gpg@arlston.com>
2019-10-31 16:59:55 +0000

    Add search by nationalInsuranceNumber

--- spec/services/prosecution_case_search_spec.rb
+++ spec/services/prosecution_case_search_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
+# rubocop:disable Metrics/BlockLength
 RSpec.describe ProsecutionCaseSearch do
   let(:params) { ActionController::Parameters.new(params_hash) }
 
@@ -46,4 +46,35 @@ RSpec.describe ProsecutionCaseSearch do
       it { is_expected.to be_empty }
     end
   end
+
+  context 'when searching by nationalInsuranceNumber' do
+    let(:cases) { FactoryBot.create_list(:prosecution_case, 2) }
+    let(:defendant) do
+      FactoryBot.build(:defendant,
+                       prosecution_case: nil,
+                       defendable: FactoryBot.create(:person_defendant,
+                                                     person: FactoryBot.create(:person, nationalInsuranceNumber: 'NH489223C')))
+    end
+
+    before do
+      cases.first.defendants << defendant
+      cases.first.save!
+    end
+
+    let(:params_hash) do
+      { nationalInsuranceNumber: 'NH489223C' }
+    end
+
+    it { is_expected.to include(cases.first) }
+    it { is_expected.not_to include(cases.second) }
+
+    context 'with a non matching reference' do
+      let(:params_hash) do
+        { nationalInsuranceNumber: 'XJ812213C' }
+      end
+
+      it { is_expected.to be_empty }
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength

ead2088f56fc25b8bb65fc7e43b618261685c07b
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-11-06 10:17:48 +0000

    add realistic factory for CustodyTimeLimits

--- spec/factories/custody_time_limits.rb
+++ spec/factories/custody_time_limits.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
   factory :custody_time_limit do
     timeLimit { '2019-10-16 14:33:00' }
     daysSpent { 10 }
+
+    factory :realistic_custody_time_limit do
+      timeLimit { DateTime.now + Faker::Types.rb_integer }
+      daysSpent { Faker::Types.rb_integer }
+    end
   end
 end

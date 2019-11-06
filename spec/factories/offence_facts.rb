49b0bca1db77063a9791ae19eec28010bf2a3057
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-11-06 14:55:29 +0000

    Add OffenceFacts realistic factory

--- spec/factories/offence_facts.rb
+++ spec/factories/offence_facts.rb
@@ -7,5 +7,13 @@ FactoryBot.define do
     alcoholReadingMethodCode { 'Random String' }
     alcoholReadingMethodDescription { 'Random String' }
     vehicleCode { 'PASSENGER_CARRYING_VEHICLE' }
+
+    factory :realistic_offence_facts do
+      vehicleRegistration { Faker::Alphanumeric.alphanumeric(7).upcase }
+      alcoholReadingAmount { Faker::Number.between(1, 15) }
+      alcoholReadingMethodCode { Faker::Alphanumeric.alphanumeric(4).upcase }
+      alcoholReadingMethodDescription { Faker::Lorem.sentence }
+      vehicleCode { OffenceFacts::VEHICLE_CODES.sample }
+    end
   end
 end
--- spec/factories/verdict_types.rb
+++ spec/factories/verdict_types.rb
@@ -6,6 +6,7 @@ FactoryBot.define do
     description { 'MyString' }
     category { 'MyString' }
     categoryType { 'MyString' }
+
     factory :realistic_verdict_type do
       add_attribute(:sequence) { Faker::Number.positive.to_i }
       description { Faker::Lorem.sentence }

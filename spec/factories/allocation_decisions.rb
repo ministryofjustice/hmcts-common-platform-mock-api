b86a1f03250d35fbb5c1951dae8336980934288f
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-11-06 14:07:44 +0000

    Add association from allocation_decision to court_indicated_sentence

--- spec/factories/allocation_decisions.rb
+++ spec/factories/allocation_decisions.rb
@@ -14,12 +14,13 @@ FactoryBot.define do
     sentencingIndicationRequested { false }
 
     factory :realistic_allocation_decision do
+      association :court_indicated_sentence, factory: :realistic_court_indicated_sentence
       originatingHearingId { SecureRandom.uuid }
       offenceId { SecureRandom.uuid }
       motReasonId { SecureRandom.uuid }
       motReasonDescription { Faker::Lorem.sentence }
       motReasonCode { Faker::Number.number(digits: 3) }
-      allocationDecisionDate { Faker::Date.forward(30) }
+      allocationDecisionDate { Faker::Date.backward }
       isSection22ALowValueShoplifting { Faker::Boolean.boolean }
       isDamageValueUnder5000 { Faker::Boolean.boolean }
       isTreatedAsIndictableOnly { Faker::Boolean.boolean }

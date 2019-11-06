9c889e536daeab29c282193d76b11dad7f2968ec
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-11-06 14:43:50 +0000

    Add jurors realistic factory

--- spec/factories/jurors.rb
+++ spec/factories/jurors.rb
@@ -5,5 +5,11 @@ FactoryBot.define do
     numberOfJurors { 1 }
     numberOfSplitJurors { 1 }
     unanimous { false }
+
+    factory :realistic_jurors do
+      numberOfJurors { Faker::Number.between(1, 12) }
+      numberOfSplitJurors { Faker::Number.between(1, 12) }
+      unanimous { Faker::Boolean.boolean }
+    end
   end
 end

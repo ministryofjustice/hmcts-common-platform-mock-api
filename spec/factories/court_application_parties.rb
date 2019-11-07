c566e414e6bccd5aff010a12571dff0aae85932a
Arlston Fernandes <gpg@arlston.com>
2019-11-07 11:19:48 +0000

    Change CourtApplicationParty defaults

--- spec/factories/court_application_parties.rb
+++ spec/factories/court_application_parties.rb
@@ -4,10 +4,5 @@ FactoryBot.define do
   factory :court_application_party do
     id { SecureRandom.uuid }
     synonym { 'MyString' }
-    person
-    organisation
-    prosecuting_authority
-    defendant
-    association :representation_organisation, factory: :organisation
   end
 end
--- spec/models/court_application_party_spec.rb
+++ spec/models/court_application_party_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe CourtApplicationParty, type: :model do
 
   describe 'associations' do
     it { should have_many(:associated_people).class_name('AssociatedPerson') }
-    it { should belong_to(:person).class_name('Person') }
-    it { should belong_to(:organisation).class_name('Organisation') }
-    it { should belong_to(:prosecuting_authority).class_name('ProsecutingAuthority') }
-    it { should belong_to(:defendant).class_name('Defendant') }
-    it { should belong_to(:representation_organisation).class_name('Organisation') }
+    it { should belong_to(:person).class_name('Person').optional }
+    it { should belong_to(:organisation).class_name('Organisation').optional }
+    it { should belong_to(:prosecuting_authority).class_name('ProsecutingAuthority').optional }
+    it { should belong_to(:defendant).class_name('Defendant').optional }
+    it { should belong_to(:representation_organisation).class_name('Organisation').optional }
   end
 
   it_has_behaviour 'conforming to valid schema'
@@ -24,7 +24,9 @@ RSpec.describe CourtApplicationParty, type: :model do
     before do
       court_application_party.person = FactoryBot.create(:person)
       court_application_party.organisation = FactoryBot.create(:organisation)
+      court_application_party.representation_organisation = FactoryBot.create(:organisation)
       court_application_party.prosecuting_authority = FactoryBot.create(:prosecuting_authority)
+      court_application_party.defendant = FactoryBot.create(:defendant)
       court_application_party.save!
     end
     it_has_behaviour 'conforming to valid schema'

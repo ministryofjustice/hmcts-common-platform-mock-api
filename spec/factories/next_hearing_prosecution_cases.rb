6e1d0cff213c5c27be6ad64f8152e4d8b596ba84
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-11-07 10:46:57 +0000

    Add realistic factory for NextHearingProsecutionCase

--- spec/factories/next_hearing_prosecution_cases.rb
+++ spec/factories/next_hearing_prosecution_cases.rb
@@ -6,4 +6,10 @@ FactoryBot.define do
       next_hearing_prosecution_case.next_hearing_defendants << FactoryBot.build(:next_hearing_defendant, next_hearing_prosecution_case: nil)
     end
   end
+
+  factory :next_hearing_prosecution_case, class: 'NextHearingProsecutionCase' do
+    after(:build) do |next_hearing_prosecution_case|
+      next_hearing_prosecution_case.next_hearing_defendants << FactoryBot.build(:realistic_next_hearing_defendant)
+    end
+  end
 end

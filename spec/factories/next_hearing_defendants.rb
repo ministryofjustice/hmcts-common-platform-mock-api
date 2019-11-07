08fe26c52b9728a9aaf6ec0453379b77b4b43501
Arlston Fernandes <gpg@arlston.com>
2019-11-07 10:45:50 +0000

    Add realistic ProsecutingAuthority

--- spec/factories/next_hearing_defendants.rb
+++ spec/factories/next_hearing_defendants.rb
@@ -5,7 +5,6 @@ FactoryBot.define do
     after(:build) do |next_hearing_defendant|
       next_hearing_defendant.next_hearing_offences << FactoryBot.build(:next_hearing_offence, next_hearing_defendant: nil)
     end
-
   end
 
   factory :realistic_next_hearing_defendant, class: 'NextHearingDefendant' do

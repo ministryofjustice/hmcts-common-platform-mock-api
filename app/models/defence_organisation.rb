3d7b0696fd4b1bad110dcff2d50a29b280f08a52
Arlston Fernandes <gpg@arlston.com>
2019-11-22 15:01:00 +0000

    Map Defendant to DefenceOrganisation and remove AssociatedDefenceOrganisation

--- app/models/defence_organisation.rb
+++ app/models/defence_organisation.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class DefenceOrganisation < ApplicationRecord
+  FUNDING_TYPES = %w[REPRESENTATION_ORDER REPRESENTATION_ORDER_APPLIED_FOR PRIVATE COURT_APPOINTED PRO_BONO].freeze
+
   belongs_to :organisation
+  belongs_to :defendant, optional: true, inverse_of: :defence_organisations
 
   validates :organisation, presence: true
   validates :laaContractNumber, presence: true, if: -> { sraNumber.blank? && barCouncilMembershipNumber.blank? }
   validates :sraNumber, presence: true, if: -> { laaContractNumber.blank? && barCouncilMembershipNumber.blank? }
   validates :barCouncilMembershipNumber, presence: true, if: -> { laaContractNumber.blank? && sraNumber.blank? }
+
+  validates :fundingType, presence: true, inclusion: FUNDING_TYPES, if: -> { defendant.present? }
+  validates :associationStartDate, presence: true, if: -> { defendant.present? }
 
   def to_builder
     Jbuilder.new do |defence_organisation|
@@ -14,6 +20,12 @@ class DefenceOrganisation < ApplicationRecord
       defence_organisation.laaContractNumber laaContractNumber if laaContractNumber.present?
       defence_organisation.sraNumber sraNumber if sraNumber.present?
       defence_organisation.barCouncilMembershipNumber barCouncilMembershipNumber if barCouncilMembershipNumber.present?
+      if defendant.present?
+        defence_organisation.fundingType fundingType
+        defence_organisation.associationStartDate associationStartDate.to_date
+        defence_organisation.associationEndDate associationEndDate.to_date
+        defence_organisation.isAssociatedByLAA isAssociatedByLAA
+      end
     end
   end
 end

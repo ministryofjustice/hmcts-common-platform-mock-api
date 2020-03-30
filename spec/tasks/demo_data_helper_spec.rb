a675622fa6fc8d1ab90dc5643f719381e6c91d0b
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-03-30 11:59:06 +0100

    add demo data helper spec

---
+++ spec/tasks/demo_data_helper_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require 'tasks/demo_data_helper'
+
+RSpec.describe DemoDataHelper, type: :helper do
+  subject(:instance) { test_class.new }
+
+  let(:test_class) do
+    Class.new do
+      include DemoDataHelper
+    end
+  end
+
+  it { is_expected.to respond_to(:prosecution_cases_by_nino, :humanize_defended_item, :humanize_prosecution_case) }
+
+  describe '#prosecution_cases_by_reference' do
+    subject(:results) { instance.prosecution_cases_by_reference(urn) }
+
+    let(:urn) { 'TESTURN123' }
+
+    before do
+      create(
+        :realistic_prosecution_case,
+        prosecution_case_identifier: create(:realistic_prosecution_case_identifier,
+                                            caseURN: urn)
+      )
+    end
+
+    it 'returns array of prosecution cases' do
+      is_expected.to all(be_instance_of(ProsecutionCase))
+    end
+
+    it 'returns matching prosecution cases' do
+      expect(results.map { |pc| pc.prosecution_case_identifier.caseURN }). to be_present.and be_all(eql('TESTURN123'))
+    end
+  end
+
+  describe '#prosecution_cases_by_nino' do
+    subject(:results) { instance.prosecution_cases_by_nino(nino) }
+
+    let(:nino) { 'JC123456A' }
+
+    before do
+      pc = create(:realistic_prosecution_case,
+                  prosecution_case_identifier: create(:realistic_prosecution_case_identifier, caseURN: 'TESTURN321'))
+      pc.defendants.first.defendable.person.update(nationalInsuranceNumber: nino)
+    end
+
+    it 'returns array of prosecution cases' do
+      is_expected.to all(be_instance_of(ProsecutionCase))
+    end
+
+    it 'returns matching prosecution cases' do
+      expect(results.map { |pc| pc.prosecution_case_identifier.caseURN }). to be_present.and be_all(eql('TESTURN321'))
+    end
+  end
+
+  describe '#humanize_defended_item' do
+    subject { instance.humanize_defended_item(defended_item) }
+
+    context 'with defendant person' do
+      let(:defended_item) { create(:realistic_defendant).defendable }
+
+      it {
+        person = defended_item.person
+        is_expected.to eql("Person: #{person.firstName}, #{person.lastName}, #{person.nationalInsuranceNumber}")
+      }
+    end
+
+    context 'with defendant organisation' do
+      let(:defended_item) { create(:realistic_defendant, :with_legal_entity_defendant).defendable }
+
+      it { is_expected.to eql("Org: #{defended_item.organisation.name}") }
+    end
+  end
+
+  describe '#humanize_prosecution_case' do
+    subject { instance.humanize_prosecution_case(prosecution_case) }
+
+    context 'with Case URN' do
+      let(:prosecution_case) do
+        create(:realistic_prosecution_case,
+               prosecution_case_identifier: create(:realistic_prosecution_case_identifier,
+                                                   caseURN: 'URN54321', prosecutionAuthorityReference: nil))
+      end
+
+      it { is_expected.to eql('URN54321') }
+    end
+
+    context 'with prosecution authority reference' do
+      let(:prosecution_case) do
+        create(:realistic_prosecution_case,
+               prosecution_case_identifier: create(:realistic_prosecution_case_identifier,
+                                                   prosecutionAuthorityReference: 'PAR54321',
+                                                   caseURN: nil))
+      end
+
+      it { is_expected.to eql('PAR54321') }
+    end
+  end
+end

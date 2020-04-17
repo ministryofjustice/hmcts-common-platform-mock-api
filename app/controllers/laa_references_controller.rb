362d7d2e3a5df1fab9c661de442ea19ef7f00eb7
Arlston Fernandes <gpg@arlston.com>
2020-04-17 15:32:08 +0100

    Change POST request responses to be 202 to better emulate CP responses

--- app/controllers/laa_references_controller.rb
+++ app/controllers/laa_references_controller.rb
@@ -3,19 +3,15 @@
 class LaaReferencesController < ApplicationController
   def record_reference
     @laa_reference = LaaReferenceRecorder.call(params)
-    head newly_created_resource? ? :created : :no_content
+    head :accepted
   end
 
   def record_representation_order
     @laa_reference = LaaRepresentationOrderRecorder.call(params)
-    head newly_created_resource? ? :created : :no_content
+    head :accepted
   end
 
   private
-
-  def newly_created_resource?
-    @laa_reference.previous_changes[:id].present?
-  end
 
   def authenticate
     send("authenticate_#{action_name}")

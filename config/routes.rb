b704e5253ca386b5fcb0d5b5ce48f2549ee11316
Arlston Fernandes <gpg@arlston.com>
2020-07-21 12:31:52 +0100

    Change URLS to match expected CP SIT format

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,14 @@ Rails.application.routes.draw do
 
   resources :prosecution_cases, path: '/prosecutionCases', only: [:index]
 
-  post '/progression-command-api' \
-    '/command/api/rest/progression/laaReference' \
+  post '/prosecutionCases/laaReference' \
     '/cases/:prosecutionCaseId' \
-    '/defendants/:defendantId' \
+    '/defendant/:defendantId' \
     '/offences/:offenceId' => 'laa_references#record_reference', as: :laa_reference
-  post '/progression-command-api' \
-    '/command/api/rest/progression/representationOrder' \
+  post '/prosecutionCases/representationOrder' \
     '/cases/:prosecutionCaseId' \
     '/defendants/:defendantId' \
     '/offences/:offenceId' => 'laa_references#record_representation_order', as: :representation_order
-  get '/LAAGetHearingHttpTrigger' => 'hearings#show', as: :hearing
-  get '/LAAGetHearingEventLogHttpTriggerFast' => 'hearings#log', as: :hearing_log
+  get '/hearing/results' => 'hearings#show', as: :hearing
+  get '/hearing/hearingLog' => 'hearings#log', as: :hearing_log
 end

a087bb08d6e9fad647749251231ed99ebbc4bfbd
hkhela <harnaik.khela@justice.gov.uk>
2020-10-20 09:20:29 +0100

    allow multiple parameters in search criteria'

--- spec/services/prosecution_case_search_spec.rb
+++ spec/services/prosecution_case_search_spec.rb
@@ -3,6 +3,12 @@
 RSpec.describe ProsecutionCaseSearch do
   let(:params) { ActionController::Parameters.new(params_hash) }
   let(:john_doe) { FactoryBot.create(:person, firstName: 'John', lastName: 'Doe', dateOfBirth: '2000-01-10') }
+  let(:defendant) do
+    build(:defendant,
+          prosecution_case: nil,
+          defendable: FactoryBot.create(:person_defendant,
+                                        person: FactoryBot.create(:person, nationalInsuranceNumber: 'nh489223c')))
+  end
 
   subject { described_class.call(params) }
 
@@ -36,6 +42,112 @@ RSpec.describe ProsecutionCaseSearch do
     it { is_expected.to include(cases.first, cases.second) }
     it { is_expected.not_to include(cases.third) }
 
+    context 'and NINO' do
+      let(:params_hash) do
+        { prosecutionCaseReference: 'XXYYZZ',
+          defendantNINO: 'NH489223C' }
+      end
+
+      before do
+        cases.first.defendants << defendant
+        cases.first.save!
+      end
+
+      it { is_expected.to include(cases.first) }
+      it { is_expected.not_to include(cases.second, cases.third) }
+    end
+
+    context 'and ASN' do
+      let(:defendant) do
+        build(:defendant,
+              prosecution_case: nil,
+              defendable: FactoryBot.create(:person_defendant, arrestSummonsNumber: '3.1428r'))
+      end
+
+      let(:defendant_two) do
+        build(:defendant,
+              prosecution_case: nil,
+              defendable: FactoryBot.create(:person_defendant, arrestSummonsNumber: '3.1428r'))
+      end
+
+      let(:params_hash) do
+        { prosecutionCaseReference: 'XXYYZZ',
+          defendantASN: '3.1428R' }
+      end
+
+      before do
+        cases.first.defendants << defendant
+        cases.first.save!
+        cases.third.defendants << defendant_two
+        cases.third.save!
+      end
+
+      it { is_expected.to include(cases.first) }
+      it { is_expected.not_to include(cases.second, cases.third) }
+    end
+
+    context 'and name and DOB' do
+      let(:defendant) do
+        build(:defendant,
+              prosecution_case: nil,
+              defendable: FactoryBot.create(:person_defendant,
+                                            person: john_doe))
+      end
+
+      let(:defendant_two) do
+        build(:defendant,
+              prosecution_case: nil,
+              defendable: FactoryBot.create(:person_defendant,
+                                            person: john_doe))
+      end
+
+      let(:params_hash) do
+        { prosecutionCaseReference: 'XXYYZZ',
+          defendantDOB: '2000-01-10', defendantName: 'John Doe' }
+      end
+
+      before do
+        cases.first.defendants << defendant
+        cases.first.save!
+        cases.third.defendants << defendant_two
+        cases.third.save!
+      end
+
+      it { is_expected.to include(cases.first) }
+      it { is_expected.not_to include(cases.second, cases.third) }
+    end
+
+    context 'and Date of next hearing' do
+      let(:defendant) do
+        build(:defendant, :with_next_hearing,
+              next_hearing_date: '2019-01-10',
+              prosecution_case: nil,
+              defendable: FactoryBot.create(:person_defendant, person: john_doe))
+      end
+
+      let(:params_hash) do
+        { prosecutionCaseReference: 'XXYYZZ',
+          dateOfNextHearing: '2019-01-10', defendantName: 'John Doe' }
+      end
+
+      let(:defendant_two) do
+        build(:defendant, :with_next_hearing,
+              next_hearing_date: '2019-01-10',
+              prosecution_case: nil,
+              defendable: FactoryBot.create(:person_defendant, person: john_doe))
+      end
+
+      before do
+        cases.first.defendants << defendant
+        cases.first.save!
+        cases.third.defendants << defendant_two
+        cases.third.save!
+      end
+
+      it { is_expected.to include(cases.first) }
+      it { is_expected.not_to include(cases.second, cases.third) }
+    end
+
     context 'with a non matching reference' do
       let(:params_hash) do
         { prosecutionCaseReference: 'NON EXISTENT' }
@@ -47,12 +159,6 @@ RSpec.describe ProsecutionCaseSearch do
 
   context 'when searching by defendantNINO' do
     let(:cases) { FactoryBot.create_list(:prosecution_case, 2) }
-    let(:defendant) do
-      build(:defendant,
-            prosecution_case: nil,
-            defendable: FactoryBot.create(:person_defendant,
-                                          person: FactoryBot.create(:person, nationalInsuranceNumber: 'nh489223c')))
-    end
 
     before do
       cases.first.defendants << defendant

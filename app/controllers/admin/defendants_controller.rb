9dbbb5c482b4b54a2f29ebd08439e82705cfbef2
hkhela <harnaik.khela@justice.gov.uk>
2020-11-25 10:11:26 +0000

    update defendant function completed

--- app/controllers/admin/defendants_controller.rb
+++ app/controllers/admin/defendants_controller.rb
@@ -1,7 +1,80 @@
 module Admin
   class DefendantsController < Admin::ApplicationController
+    before_action :set_defendant, only: %i[show edit update]
+
     def show
+    end
+
+    def edit
+    end
+
+    def update
+      if @defendant.update(defendant_params)
+        redirect_to [:admin, @defendant], notice: 'Defendant was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def set_defendant
       @defendant = Defendant.find(params[:id])
+    end
+
+    def defendant_params
+      params.require(:defendant).permit(
+       :numberOfPreviousConvictionsCited,
+       :prosecutionAuthorityReference,
+       :witnessStatement,
+       :witnessStatementWelsh,
+       :mitigation,
+       :mitigationWelsh,
+       :croNumber,
+       :pncId,
+       :mergedProsecutionCaseReference,
+       :masterDefendantId,
+       :courtProceedingsInitiated,
+       :defendantDetailsUpdated,
+       :defendable_type,
+       { defendable_attributes: defendable_attributes})
+    end
+
+    def defendable_attributes
+      [:id,
+       :bailConditions,
+       :bailReasons,
+       :perceivedBirthYear,
+       :driverNumber,
+       :driverLicenceCode,
+       :driverLicenseIssue,
+       :vehicleOperatorLicenceNumber,
+       :arrestSummonsNumber,
+       :employerPayrollReference,
+       { person_attributes: person_attributes }]
+    end
+
+    def person_attributes
+      %i[id
+         title
+         firstName
+         middleName
+         lastName
+         dateOfBirth
+         nationalityId
+         nationalityCode
+         nationalityDescription
+         additionalNationalityId
+         additionalNationalityCode
+         additionalNationalityDescription
+         disabilityStatus
+         gender
+         interpreterLanguageNeeds
+         documentationLanguageNeeds
+         nationalInsuranceNumber
+         occupation
+         occupationCode
+         specificRequirements]
     end
   end
 end
--- spec/requests/admin/defendants_request_spec.rb
+++ spec/requests/admin/defendants_request_spec.rb
@@ -5,11 +5,50 @@ RSpec.describe "Admin::Defendants", type: :request do
   let(:defendant) { FactoryBot.create(:defendant) }
   let(:headers) { { 'Authorization': authorisation } }
   let(:authorisation) { ActionController::HttpAuthentication::Basic.encode_credentials(ENV['ADMIN_USERNAME'], ENV['ADMIN_PASSWORD']) }
-  
+  let(:invalid_attributes) do
+    FactoryBot.attributes_for(:defendant,
+                              defendable_type: 'PersonDefendant',
+                              defendable_attributes: FactoryBot.attributes_for(:person_defendant,
+                                                                               person_attributes: FactoryBot.attributes_for(:person, lastName: nil)))
+  end
+
+
   describe "GET /show" do
     it "returns http success" do
       get "/admin/defendants/#{defendant.id}", headers: headers
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/admin/defendants/#{defendant.id}/edit", headers: headers
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
+      let(:new_attributes) { FactoryBot.attributes_for(:realistic_defendant) }
+
+      it 'updates the requested defendant' do
+        patch admin_defendant_url(defendant), params: { defendant: new_attributes }, headers: headers
+        defendant.reload
+        expect(defendant).to have_attributes(new_attributes)
+      end
+
+      it 'redirects to the defendant' do
+        patch admin_defendant_url(defendant), params: { defendant: new_attributes }, headers: headers
+        defendant.reload
+        expect(response).to redirect_to(admin_defendant_url(defendant))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it "renders a successful response (i.e. to display the 'edit' template)" do
+        patch admin_defendant_url(defendant), params: { defendant: invalid_attributes }, headers: headers
+        expect(response).to be_successful
+      end
     end
   end
 end

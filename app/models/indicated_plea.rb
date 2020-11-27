bd7a501eeae2fb33d1858ed78f3e89ed13c7964a
Arlston Fernandes <gpg@arlston.com>
2020-11-27 17:23:52 +0000

    Refactor to link indicated plea against hearing and offence

--- app/models/indicated_plea.rb
+++ app/models/indicated_plea.rb
@@ -4,15 +4,17 @@ class IndicatedPlea < ApplicationRecord
   VALUES = %w[INDICATED_GUILTY INDICATED_NOT_GUILTY NO_INDICATION].freeze
   SOURCES = %w[ONLINE IN_COURT].freeze
 
-  validates :offenceId, presence: true
+  belongs_to :hearing
+  belongs_to :offence
+
   validates :indicatedPleaDate, presence: true
   validates :indicatedPleaValue, presence: true, inclusion: VALUES
   validates :source, presence: true, inclusion: SOURCES
 
   def to_builder
     Jbuilder.new do |indicated_plea|
-      indicated_plea.offenceId offenceId
-      indicated_plea.originatingHearingId originatingHearingId
+      indicated_plea.offenceId offence_id
+      indicated_plea.originatingHearingId hearing_id
       indicated_plea.indicatedPleaDate indicatedPleaDate.to_date
       indicated_plea.indicatedPleaValue indicatedPleaValue
       indicated_plea.source source

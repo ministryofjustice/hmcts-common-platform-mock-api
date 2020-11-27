676c235e39feffddbc898dd3b774fd3783712cc2
Arlston Fernandes <gpg@arlston.com>
2020-11-27 17:23:31 +0000

    Refactor to link plea against hearing and offence

--- spec/models/plea_spec.rb
+++ spec/models/plea_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe Plea, type: :model do
 
   subject { plea }
 
-  it { should validate_presence_of(:originatingHearingId) }
-  it { should validate_presence_of(:offenceId) }
-  it { should validate_presence_of(:pleaDate) }
-  it { should validate_presence_of(:pleaValue) }
+  it { is_expected.to validate_presence_of(:pleaDate) }
+  it { is_expected.to validate_presence_of(:pleaValue) }
   it do
-    should validate_inclusion_of(:pleaValue)
+    is_expected.to validate_inclusion_of(:pleaValue)
       .in_array(%w[GUILTY NOT_GUILTY])
   end
 
-  it { should belong_to(:delegated_powers).class_name('DelegatedPowers').optional }
+  it { is_expected.to belong_to(:delegated_powers).class_name('DelegatedPowers').optional }
+  it { is_expected.to belong_to(:offence) }
+  it { is_expected.to belong_to(:hearing) }
 
   it_has_behaviour 'conforming to valid schema'
 

918714329d242ca1565cf8d2cf960b5fffd62a3c
Arlston Fernandes <gpg@arlston.com>
2020-11-30 16:29:25 +0000

    Enable creation of pleas against hearings/offences

--- app/controllers/admin/hearings_controller.rb
+++ app/controllers/admin/hearings_controller.rb
@@ -3,7 +3,7 @@
 module Admin
   class HearingsController < Admin::ApplicationController
     before_action :set_prosecution_case, only: %i[new create]
-    before_action :set_hearing, only: %i[show edit update destroy]
+    before_action :set_hearing, only: %i[show edit update destroy add_plea]
 
     def show; end
 
@@ -36,6 +36,12 @@ module Admin
       redirect_to [:admin, @prosecution_case], notice: 'Hearing was successfully destroyed.'
     end
 
+    def add_plea
+      @offence = Offence.find(params[:offence_id])
+      @offence.pleas.create(pleaDate: Time.zone.now, pleaValue: Plea::VALUES.first, hearing: @hearing)
+      redirect_to edit_admin_hearing_url(@hearing), notice: 'Plea was successfully added.'
+    end
+
     private
 
     # Use callbacks to share common setup or constraints between actions.
@@ -58,8 +64,21 @@ module Admin
                                       :hasSharedResults,
                                       :isEffectiveTrial,
                                       :isBoxHearing,
+                                      { prosecution_cases_attributes: prosecution_cases_attributes },
                                       { hearing_type_attributes: %i[id description] },
                                       { hearing_days_attributes: %i[id sittingDay listedDurationMinutes] })
+    end
+
+    def prosecution_cases_attributes
+      [:id, { defendants_attributes: defendant_attributes }]
+    end
+
+    def defendant_attributes
+      [:id, { offences_attributes: offences_attributes }]
+    end
+
+    def offences_attributes
+      [:id, { pleas_attributes: %i[id hearing_id pleaValue pleaDate _destroy] }]
     end
   end
 end
--- spec/requests/admin/hearings_spec.rb
+++ spec/requests/admin/hearings_spec.rb
@@ -106,4 +106,19 @@ RSpec.describe '/admin/hearings', type: :request do
       expect(response).to redirect_to(admin_prosecution_case_url(prosecution_case))
     end
   end
+
+  describe 'GET /add_plea' do
+    let(:offence) { prosecution_case.defendants.first.offences.first }
+
+    it 'creates a new plea' do
+      expect do
+        post add_plea_admin_hearing_url(hearing, offence), headers: headers
+      end.to change(offence.pleas, :count).by(1)
+    end
+
+    it 'redirects to the edit hearings page' do
+      post add_plea_admin_hearing_url(hearing, offence), headers: headers
+      expect(response).to redirect_to(edit_admin_hearing_url(hearing))
+    end
+  end
 end

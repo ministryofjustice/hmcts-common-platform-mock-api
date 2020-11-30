6692b0ebc0f548bd675ce962d2f4a4cb016538ad
hkhela <harnaik.khela@justice.gov.uk>
2020-11-30 18:22:17 +0000

    Create new defendant feature added

--- app/controllers/admin/defendants_controller.rb
+++ app/controllers/admin/defendants_controller.rb
@@ -8,6 +8,22 @@ module Admin
     def edit
     end
 
+    def new
+      @prosecution_case = ProsecutionCase.find(params[:prosecution_case_id])
+      @defendant = FactoryBot.build(:realistic_defendant)
+    end
+
+    def create
+      @prosecution_case = ProsecutionCase.find(params[:prosecution_case_id])
+      @defendant = @prosecution_case.defendants.build(defendant_params)
+
+      if @defendant.save    
+        redirect_to [:admin, @defendant], notice: 'Defendant was successfully created.'
+      else
+        render :new
+      end
+    end
+
     def update
       if @defendant.update(defendant_params)
         redirect_to [:admin, @defendant], notice: 'Defendant was successfully updated.'
@@ -42,7 +58,8 @@ module Admin
        :courtProceedingsInitiated,
        :defendantDetailsUpdated,
        :defendable_type,
-       { defendable_attributes: defendable_attributes})
+       { defendable_attributes: defendable_attributes,
+       offences_attributes: offences_attributes})
     end
 
     def defendable_attributes
@@ -80,6 +97,69 @@ module Admin
          occupation
          occupationCode
          specificRequirements]
+    end
+
+    def offences_attributes
+      [:id,
+       :offenceDefinitionId,
+       :offenceCode,
+       :offenceTitle,
+       :offenceTitleWelsh,
+       :offenceLegislation,
+       :offenceLegislationWelsh,
+       :modeOfTrial,
+       :wording,
+       :wordingWelsh,
+       :startDate,
+       :endDate,
+       :arrestDate,
+       :chargeDate,
+       :dateOfInformation,
+       :orderIndex,
+       :count,
+       :convictionDate,
+       :aquittalDate,
+       :isDisposed,
+       :isDiscontinued,
+       :isIntroduceAfterInitialProceedings,
+       :splitProsecutorCaseReference,
+       :mergedProsecutionCaseReference,
+       :dvlaCode,
+       :laidDate,
+       :proceedingsConcluded,
+       :pendingCBPW,
+       :civilOffence,
+       :_destroy,
+       { judicial_results_attributes: judicial_results_attributes }]
+    end
+
+    def judicial_results_attributes
+      %i[
+        id
+        judicialResultId
+        judicialResultTypeId
+        orderedHearingId
+        label
+        welshLabel
+        isAdjournmentResult
+        isFinancialResult
+        isConvictedResult
+        isAvailableForCourtExtract
+        isDeleted
+        amendmentReasonId
+        amendmentReason
+        amendmentDate
+        qualifier
+        resultText
+        cjsCode
+        rank
+        orderedDate
+        postHearingCustodyStatus
+        lastSharedDateTime
+        terminatesOffenceProceedings
+        approvedDate
+        category
+      ]
     end
   end
 end
--- spec/requests/admin/defendants_request_spec.rb
+++ spec/requests/admin/defendants_request_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Admin::Defendants", type: :request do
 
+  let(:prosecution_case) { FactoryBot.create(:prosecution_case) }
   let(:defendant) { FactoryBot.create(:defendant) }
   let(:headers) { { 'Authorization': authorisation } }
   let(:authorisation) { ActionController::HttpAuthentication::Basic.encode_credentials(ENV['ADMIN_USERNAME'], ENV['ADMIN_PASSWORD']) }
@@ -12,7 +13,14 @@ RSpec.describe "Admin::Defendants", type: :request do
                                                                                person_attributes: FactoryBot.attributes_for(:person, lastName: nil)))
   end
 
-
+  let(:valid_attributes) do
+    FactoryBot.attributes_for(:defendant,
+                              defendable_type: 'PersonDefendant',
+                              defendable_attributes: FactoryBot.attributes_for(:person_defendant,
+                                                                               person_attributes: FactoryBot.attributes_for(:person)),
+                                                                               offences_attributes: [FactoryBot.attributes_for(:offence)])
+  end
+  
   describe "GET /show" do
     it "returns http success" do
       get "/admin/defendants/#{defendant.id}", headers: headers
@@ -64,6 +72,43 @@ RSpec.describe "Admin::Defendants", type: :request do
     it 'redirects to the defendant list' do
       delete admin_defendant_url(defendant), headers: headers
       expect(response).to redirect_to(admin_prosecution_case_url(defendant.prosecution_case))
+    end
+  end 
+
+  describe 'GET /new' do
+    it 'renders a successful response' do
+      get new_admin_prosecution_case_defendant_url(prosecution_case), headers: headers
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    before { prosecution_case.save! }
+    context 'with valid parameters' do
+
+      it 'creates a new Defendant' do
+        expect do
+          post admin_prosecution_case_defendants_url(prosecution_case), params: { defendant: valid_attributes }, headers: headers
+        end.to change(Defendant, :count).by(1)
+      end
+
+      it 'redirects to the created defendant' do
+        post admin_prosecution_case_defendants_url(prosecution_case), params: { defendant: valid_attributes }, headers: headers
+        expect(response).to redirect_to(admin_defendant_url(Defendant.order(created_at: :asc).last))
+      end
+    end
+
+    context 'with invalid parameters' do
+      it 'does not create a new Defendant' do
+        expect do
+          post admin_prosecution_case_defendants_url(prosecution_case), params: { defendant: invalid_attributes }, headers: headers
+        end.to change(Defendant, :count).by(0)
+      end
+
+      it "renders a successful response (i.e. to display the 'new' template)" do
+        post admin_prosecution_case_defendants_url(prosecution_case), params: { defendant: invalid_attributes }, headers: headers
+        expect(response).to be_successful
+      end
     end
   end
 

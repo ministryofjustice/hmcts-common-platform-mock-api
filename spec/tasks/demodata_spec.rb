dc8932ff21a667c907aac4e4a054c94b3a054d08
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-12-02 00:09:21 +0000

    Extend and refactor spec
    Cover plea and allocation decision creation

--- spec/tasks/demodata_spec.rb
+++ spec/tasks/demodata_spec.rb
@@ -38,32 +38,39 @@ RSpec.describe 'Demo data tasks', type: :rake do
       expect { loader }.to change(ProsecutionCase, :count).by(2)
     end
 
+    it 'creates 1 allocation_decision' do
+      expect { loader }.to change(AllocationDecision, :count).by(1)
+    end
+
+    it 'creates 2 pleas' do
+      expect { loader }.to change(Plea, :count).by(2)
+    end
+
     it 'creates 2 defence counsels' do
       expect { loader }.to change(DefenceCounsel, :count).by(2)
     end
 
     context 'for case 1' do
       before { loader }
+
       let(:case1) { prosecution_cases_by_reference('TEST12345').first }
 
       it { expect(case1.prosecution_case_identifier.caseURN).to eql 'TEST12345' }
 
-      context 'with hearings' do
-        it 'adds 3 hearings' do
-          expect(case1.hearings.count).to eql 3
-        end
+      it 'adds 3 hearings' do
+        expect(case1.hearings.count).to be 3
+      end
 
-        it 'all hearings are "resulted"' do
-          expect(case1.hearings.pluck(:resulted)).to all(be(true))
-        end
+      it 'all hearings are "resulted"' do
+        expect(case1.hearings.pluck(:resulted)).to all(be(true))
+      end
 
-        it 'adds 3 hearing days to each hearing' do
-          expect(case1.hearings.map(&:hearing_days).map(&:size)).to eql([3, 3, 3])
-        end
+      it 'adds 3 hearing days to each hearing' do
+        expect(case1.hearings.map(&:hearing_days).map(&:size)).to eql([3, 3, 3])
+      end
 
-        it 'adds 10 hearing events to each hearing day' do
-          expect(case1.hearings.flat_map(&:hearing_days).map(&:events).map(&:size)).to eql(Array.new(9, 10))
-        end
+      it 'adds 10 hearing events to each hearing day' do
+        expect(case1.hearings.flat_map(&:hearing_days).map(&:events).map(&:size)).to eql(Array.new(9, 10))
       end
     end
   end

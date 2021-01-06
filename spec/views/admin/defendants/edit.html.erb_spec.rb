a4f29f0d0303663f29a9f760a04151674eebb5f5
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2021-01-06 17:58:30 +0000

    adding test content to view defendant edit

--- spec/views/admin/defendants/edit.html.erb_spec.rb
+++ spec/views/admin/defendants/edit.html.erb_spec.rb
@@ -2,6 +2,17 @@
 
 require "rails_helper"
 
-RSpec.describe "defendants/edit.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "admin/defendants/edit.html.erb", type: :view do
+  let(:defendant) { FactoryBot.create(:defendant) }
+
+  before do
+    assign(:defendant, defendant)
+  end
+
+  it "renders the edit defendant form" do
+    render
+
+    assert_select "form[action=?][method=?]", admin_defendant_path(defendant), "post" do
+    end
+  end
 end

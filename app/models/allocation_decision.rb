bc634d548cd7d1c6754fd95645d1cbf50a880d00
Chris Groves <chris_groves87@hotmail.co.uk>
2021-01-12 10:06:22 +0000

    Remove allocation reason id field from view and create hash of MOT reason descriptions and corresponding reason code

--- app/models/allocation_decision.rb
+++ app/models/allocation_decision.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 class AllocationDecision < ApplicationRecord
-  VALUES = %w[Offence_triable_only_by_court_martial
-              Indictable_only_offence
-              No_mode_of_Trial_Either_way_offence
-              Defendant_consents_to_summary_trial
-              Commanding_Officer_orders_trial_by_court_martial
-              Defendant_chooses_trial_by_court_martial_1
-              Defendant_elects_trial_by_jury
-              Summary_only_offence
-              Youth_offence_triable_on_indictment_only_(homicide_relevant_firearms_dangerousness)
-              Youth_Court_directs_trial_by_jury_(grave_crime)
-              Low_value_offence_triable_summarily_only
-              Indictable_only_(previous_convictions_relevant_firearms_offence)
-              Court_directs_trial_by_jury].freeze
+  VALUES = { "Offence triable only by court martial": 8,
+             "Indictable only offence": 2,
+             "No mode of Trial - Either way offence": 7,
+             "Defendant consents to summary trial": 3,
+             "Commanding Officer orders trial by court martial": 10,
+             "Defendant chooses trial by court martial 1": 9,
+             "Defendant elects trial by jury": 4,
+             "Summary - only offence": 1,
+             "Youth - offence triable on indictment only (homicide / relevant firearms / dangerousness)": 2,
+             "Youth - Court directs trial by jury (grave crime)": 5,
+             "Low value offence triable summarily only": 6,
+             "Indictable only (previous convictions / relevant firearms offence)": 2,
+             "Court directs trial by jury": 5 }.freeze
 
   belongs_to :court_indicated_sentence, optional: true
 

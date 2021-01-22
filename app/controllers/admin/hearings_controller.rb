b15c4d8bd71f58985cb86a16dbc825af85ac5a7a
Chris Groves <chris_groves87@hotmail.co.uk>
2021-01-22 17:16:05 +0000

    Change default mode of trial reason code to 0

--- app/controllers/admin/hearings_controller.rb
+++ app/controllers/admin/hearings_controller.rb
@@ -44,7 +44,7 @@ module Admin
 
     def add_allocation_decision
       @offence = Offence.find(params[:offence_id])
-      @offence.allocation_decisions.create!(allocationDecisionDate: Time.zone.now, motReasonId: SecureRandom.uuid, motReasonCode: "ABCD", motReasonDescription: Faker::Offence.mode_of_trial_reason, hearing: @hearing)
+      @offence.allocation_decisions.create!(allocationDecisionDate: Time.zone.now, motReasonId: SecureRandom.uuid, motReasonCode: 0, motReasonDescription: Faker::Offence.mode_of_trial_reason, hearing: @hearing)
       redirect_to edit_admin_hearing_url(@hearing), notice: "Allocation decision was successfully added."
     end
 

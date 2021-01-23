e8185839d21ff780da4f46bb2205a067b8b051cc
Chris Groves <chris_groves87@hotmail.co.uk>
2021-01-23 20:01:43 +0000

    Add ability to add an allocation decision against an offence

--- app/models/allocation_decision.rb
+++ app/models/allocation_decision.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 class AllocationDecision < ApplicationRecord
+  DESCRIPTIONS = %w[Offence_triable_only_by_court_martial
+                    Indictable_only_offence
+                    No_mode_of_Trial_Either_way_offence
+                    Defendant_consents_to_summary_trial
+                    Commanding_Officer_orders_trial_by_court_martial
+                    Defendant_chooses_trial_by_court_martial_1
+                    Defendant_elects_trial_by_jury
+                    Summary_only_offence
+                    Youth_offence_triable_on_indictment_only_(homicide_relevant_firearms_dangerousness)
+                    Youth_Court_directs_trial_by_jury_(grave_crime)
+                    Low_value_offence_triable_summarily_only
+                    Indictable_only_(previous_convictions_relevant_firearms_offence)
+                    Court_directs_trial_by_jury].freeze
+
+  CODES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].freeze
+
   belongs_to :court_indicated_sentence, optional: true
 
   belongs_to :hearing

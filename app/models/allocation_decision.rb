01bc4e60c12b96b0f313883df20542f3f7a75ac5
Chris Groves <chris_groves87@hotmail.co.uk>
2021-01-25 11:43:04 +0000

    Remove under scores from mode of trial reason descriptions

--- app/models/allocation_decision.rb
+++ app/models/allocation_decision.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 class AllocationDecision < ApplicationRecord
-  DESCRIPTIONS = %w[Offence_triable_only_by_court_martial
-                    Indictable_only_offence
-                    No_mode_of_Trial_Either_way_offence
-                    Defendant_consents_to_summary_trial
-                    Commanding_Officer_orders_trial_by_court_martial
-                    Defendant_chooses_trial_by_court_martial_1
-                    Defendant_elects_trial_by_jury
-                    Summary_only_offence
-                    Youth_offence_triable_on_indictment_only_(homicide_relevant_firearms_dangerousness)
-                    Youth_Court_directs_trial_by_jury_(grave_crime)
-                    Low_value_offence_triable_summarily_only
-                    Indictable_only_(previous_convictions_relevant_firearms_offence)
-                    Court_directs_trial_by_jury].freeze
+  DESCRIPTIONS = ["Offence triable only by court martial",
+                  "Indictable only offence",
+                  "No mode of Trial Either way offence",
+                  "Defendant consents to summary trial",
+                  "Commanding Officer orders trial by court martial",
+                  "Defendant chooses trial by court martial 1",
+                  "Defendant elects trial by jury",
+                  "Summary only offence",
+                  "Youth offence triable on indictment only (homicide relevant firearms dangerousness)",
+                  "Youth Court directs trial by jury (grave crime)",
+                  "Low value offence triable summarily only",
+                  "Indictable only (previous convictions relevant firearms offence)",
+                  "Court directs trial by jury"].freeze
 
   CODES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].freeze
 

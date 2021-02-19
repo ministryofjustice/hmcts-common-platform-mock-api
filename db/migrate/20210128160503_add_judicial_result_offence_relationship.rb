4f5a0a2d5e0a55aa9a487cf30893044bcc7c0230
Laurent Curau <laurent.curau@digital.justice.gov.uk>
2021-02-19 11:26:13 +0100

    Use destroy_all instead of delete_all
    delete_all doesn't trigger callbacks (so no dependent: :destroy),
    whereas destroy_all will.
    
    https://apidock.com/rails/v6.0.0/ActiveRecord/Relation/destroy_all

--- db/migrate/20210128160503_add_judicial_result_offence_relationship.rb
+++
@@ -1,7 +0,0 @@
-class AddJudicialResultOffenceRelationship < ActiveRecord::Migration[6.0]
-  def up
-    JudicialResult.delete_all
-
-    add_reference :judicial_results, :hearing, type: :uuid, null: false, foreign_key: true
-  end
-end

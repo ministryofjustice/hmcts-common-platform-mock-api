26c2acc3b0033e293be0b23f4d25d1ecbd0aa9ca
Chris Groves <chris_groves87@hotmail.co.uk>
2021-03-11 09:48:39 +0000

    Amend factory data for Committing Courts model

--- spec/factories/committing_courts.rb
+++ spec/factories/committing_courts.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
 
     factory :realistic_committing_court do
       court_centre_id { "6131bd34-33d9-3d1e-8152-8b5a2084f1bd" }
-      court_house_type { "CROWN" }
+      court_house_type { CommittingCourt::COURT_TYPES.sample }
       court_house_code { "DE" }
       court_house_name { "Derby Crown Court" }
       court_house_short_name { "DE" }

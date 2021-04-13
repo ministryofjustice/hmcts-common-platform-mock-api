fe0201e48f299345113e1ee21c2d063d3917bd01
Chris Groves <chris_groves87@hotmail.co.uk>
2021-04-13 12:34:24 +0100

    Fix migration error

--- db/migrate/20200416124338_create_company_representatives.rb
+++ db/migrate/20200416124338_create_company_representatives.rb
@@ -2,14 +2,11 @@ class CreateCompanyRepresentatives < ActiveRecord::Migration[6.0]
   def change
     create_table :company_representatives, id: :uuid do |t|
       t.string :title
-      t.string :first_name
-      t.string :last_name
+      t.string :firstName
+      t.string :lastName
       t.string :position
-      t.uuid :defendants, array: true, default: []
-      t.text :attendance_days, array: true, default: []
 
       t.timestamps
     end
-
   end
 end

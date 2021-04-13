1e5633485ac31f92bbdc1120c8771185c760a209
Chris Groves <chris_groves87@hotmail.co.uk>
2021-04-13 12:07:46 +0100

    Re add deleted migration file

---
+++ db/migrate/20191023152751_create_court_application_outcomes.rb
@@ -0,0 +1,11 @@
+class CreateCourtApplicationOutcomes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :court_application_outcomes, id: :uuid do |t|
+      t.uuid :originatingHearingId
+      t.uuid :applicationId
+      t.datetime :applicationOutcomeDate
+      t.references :application_outcome_type, type: :uuid, foreign_key: { to_table: :court_application_outcome_types }
+      t.timestamps
+    end
+  end
+end

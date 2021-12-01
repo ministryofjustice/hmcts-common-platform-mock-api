166211644993267968937b78907068236f09a8d7
Arthur Ashman <arthur.ashman@digital.justice.gov.uk>
2021-12-01 19:09:44 +0000

    use local sample list of court centres for dropdowns

--- app/models/concerns/court_centre_relatable.rb
+++ app/models/concerns/court_centre_relatable.rb
@@ -7,7 +7,7 @@ module CourtCentreRelatable
     def court_centre
       return if court_centre_id.blank?
 
-      @court_centre ||= CourtCentre.new(id: court_centre_id)
+      @court_centre ||= CourtCentre.find_by(id: court_centre_id)
     end
   end
 end

c90ee3cd318a8d0adbd252285670fff110e6eb26
Chris Groves <chris_groves87@hotmail.co.uk>
2021-12-13 08:11:01 +0000

    Remove prosecution case controller specs

--- spec/requests/prosecution_cases_spec.rb
+++ spec/requests/prosecution_cases_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe "ProsecutionCases", type: :request do
-  let(:headers) { { 'Ocp-Apim-Subscription-Key': ENV.fetch("SHARED_SECRET_KEY") } }
-
   describe "GET /prosecutionCases" do
     let!(:prosecution_case) do
       FactoryBot.create(:prosecution_case,
@@ -10,18 +8,29 @@ RSpec.describe "ProsecutionCases", type: :request do
                                                                        caseURN: "some-reference"))
     end
 
-    it "matches the response schema" do
-      get "/prosecutionCases?prosecutionCaseReference=some-reference", headers: headers
-      expect(response).to have_http_status(:ok)
-      expect(response.headers["content-type"]).to eq("application/vnd.unifiedsearch.query.laa.cases+json")
-      expect(response.body).to match_json_schema(:search_prosecution_case_response)
+    context "when the auth header is incorrect" do
+      it "returns a response with an unauthorised status" do
+        get "/prosecutionCases?prosecutionCaseReference=some-reference"
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
 
-    context "when the search returns no results" do
-      it "returns a response with an ok status" do
-        get "/prosecutionCases?prosecutionCaseReference=incorrect-reference", headers: headers
+    context "when the auth header is correct" do
+      let(:headers) { { 'Ocp-Apim-Subscription-Key': ENV.fetch("SHARED_SECRET_KEY") } }
+
+      it "matches the response schema" do
+        get "/prosecutionCases?prosecutionCaseReference=some-reference", headers: headers
         expect(response).to have_http_status(:ok)
         expect(response.headers["content-type"]).to eq("application/vnd.unifiedsearch.query.laa.cases+json")
+        expect(response.body).to match_json_schema(:search_prosecution_case_response)
+      end
+
+      context "when the search returns no results" do
+        it "returns a response with an ok status" do
+          get "/prosecutionCases?prosecutionCaseReference=incorrect-reference", headers: headers
+          expect(response).to have_http_status(:ok)
+          expect(response.headers["content-type"]).to eq("application/vnd.unifiedsearch.query.laa.cases+json")
+        end
       end
     end
   end

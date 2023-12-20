2872cdc6d8aa576527c5b2a681c77d03db22539b
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2023-12-20 10:44:34 +0000

    LASB-2638: Allow factory bot to reject primary key attributes in testing.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.factory_bot.reject_primary_key_attributes = false
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
